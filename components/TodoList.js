c20c9338cd5bb656e288b11c22177ffd491d63b6
Mochilin Vladislav <zeroour@yandex.ru>
2022-11-06 18:48:29 +0300

    cleaned console log

--- components/TodoList.js
+++ components/TodoList.js
@@ -29,8 +29,6 @@ function TodoList() {
         setTasks((prev) => prev.map((task) => (task.id === taskId ? newValue : task)));
     }
 
-    console.log(tasks)
-
     return (
         <div className='to-do-list'>
             <h1>Список задач</h1>
